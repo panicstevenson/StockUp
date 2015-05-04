6f0ee2f55e7510d67b564f216fc1415d1862e468
Panic Stevenson <panic.stevenson@gmail.com>
2015-05-03 23:13:23 -0700

    Delete and Gestures

--- app/src/main/java/citris/stockup/grocerylist/WaitSplash.java
+++ app/src/main/java/citris/stockup/grocerylist/WaitSplash.java
@@ -14,7 +14,7 @@ import citris.stockup.R;
 public class WaitSplash extends Activity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
